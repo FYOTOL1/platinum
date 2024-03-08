77701cc859c218dedf2ea0fea716c520c269c59d
Ahmed <fyotol@gmail.com>
2024-03-08 10:16:03 -0800

    First Deploy

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,225 @@
+import React, { useEffect, useState } from "react";
+
+export default function Header() {
+  const [Search, setSearch] = useState("");
+  const [IsLang, setIsLang] = useState(false);
+  const [IsCoin, setIsCoin] = useState(false);
+  const [IsAvatar, setIsAvatar] = useState(false);
+  const [Coin, setCoin] = useState("");
+  const [Lang, setLang] = useState("");
+
+  const coins_type = [
+    "SAR",
+    "USD",
+    "EG",
+    "EUR",
+    "OMR",
+    "KWD",
+    "BHD",
+    "MAD",
+    "TRY",
+    "QAR",
+    "AED",
+  ];
+
+  const ActiveCoin = () => {
+    setIsCoin(!IsCoin);
+    setIsLang(false);
+    setIsAvatar(false);
+  };
+  const ActiveLang = () => {
+    setIsLang(!IsLang);
+    setIsCoin(false);
+    setIsAvatar(false);
+  };
+  const ActiveAvatar = () => {
+    setIsAvatar(!IsAvatar);
+    setIsLang(false);
+    setIsCoin(false);
+  };
+
+  return (
+    <>
+      <header className="lg:container flex items-center flex-row-reverse justify-between mx-auto py-3 px-2 overflow-x-scroll md:overflow-visible">
+        {/* Logo */}
+        <img
+          className="w-24 sm:w-32 md:w-40 lg:w-56"
+          src="/logo.svg"
+          alt="Logo"
+        />
+        {/* Search Bar */}
+        <div className="relative w-full min-w-[200px] max-w-[750px] h-[25px] sm:h-[35px] md:h-[45px] lg:h-[50px] sm:text-xs md:text-base">
+          <label
+            className="absolute right-4 md:right-6 top-[50%] translate-y-[-50%] text-[12px] sm:text-[14px]"
+            htmlFor="search"
+          >
+            <i className="fa-solid fa-magnifying-glass fa-fw text-gray-400"></i>
+          </label>
+          <input
+            id="search"
+            onChange={(e) => setSearch(e.target.value)}
+            value={Search}
+            className="pe-10 md:pe-14 ps-12 sm:ps-14 md:ps-16 h-full w-full text-end text-sm rounded-full [box-shadow:_0_2px_4px_rgba(0,0,0,.04),_0_0_4px_rgba(0,0,0,.06)] outline-none"
+            type="text"
+            placeholder="البحث عن الفعالية او الفئة"
+          />
+          {Search !== "" && (
+            <label
+              className="absolute left-3 sm:left-4 md:left-5 top-[50%] translate-y-[-55%] text-xs sm:text-sm md:text-base"
+              htmlFor="search"
+            >
+              <button
+                onClick={() => setSearch("")}
+                className="text-blue-500 transition-all hover:text-blue-300"
+              >
+                <p>مسح</p>
+              </button>
+            </label>
+          )}
+        </div>
+        {/* options */}
+        <div className="flex items-center justify-between flex-row-reverse gap-2 sm:gap-4 md:gap-6 me-2 sm:me-4 md:me-6 lg:me-8 w-full">
+          {/* Date */}
+          <label className="text-lg md:text-xl lg:text-2xl" htmlFor="date">
+            <i className="fa-regular fa-calendar"></i>
+          </label>
+          {/* Language */}
+          <div className="Lang">
+            <button
+              className={`relative flex items-baseline flex-row-reverse gap-1 text-[9px] sm:text-[12px] md:text-sm transition-all ${
+                IsLang ? "text-blue-500" : "text-gray-500"
+              }`}
+              onClick={() => ActiveLang()}
+            >
+              <i className="fa-solid fa-globe ms-1"></i>
+              <p className="capitalize">{Lang || "english"}</p>
+              <i
+                className={`fa-solid fa-angle-down transition-all duration-300 ${
+                  IsLang ? "rotate-180" : "rotate-0"
+                }`}
+              ></i>
+
+              {IsLang ? (
+                <div className="absolute mt-10 left-[-150px] w-[250px] p-3 bg-white shadow-md outline outline-1 outline-gray-100 rounded-xl">
+                  <ul className="flex flex-col text-lg text-end">
+                    <li
+                      onClick={() => setLang("english")}
+                      className={`w-full py-3 transition-all ${
+                        Lang == "english" ? "text-blue-600" : "text-gray-500"
+                      } font-semibold hover:bg-blue-100 hover:text-blue-600 px-4 rounded-lg `}
+                    >
+                      English
+                    </li>
+                    <li
+                      onClick={() => setLang("عربية")}
+                      className={`w-full py-3 transition-all ${
+                        Lang == "عربية" ? "text-blue-600" : "text-gray-500"
+                      } font-semibold hover:bg-blue-100 hover:text-blue-600 px-4 rounded-lg `}
+                    >
+                      عربية
+                    </li>
+                    <li
+                      onClick={() => setLang("Türkçe")}
+                      className={`w-full py-3 transition-all ${
+                        Lang == "Türkçe" ? "text-blue-600" : "text-gray-500"
+                      } font-semibold hover:bg-blue-100 hover:text-blue-600 px-4 rounded-lg `}
+                    >
+                      Türkçe
+                    </li>
+                  </ul>
+                </div>
+              ) : (
+                ""
+              )}
+            </button>
+          </div>
+          {/* Money Type */}
+          <div className="Money">
+            <button
+              className={`relative flex items-baseline flex-row-reverse gap-1 text-[9px] sm:text-[12px] md:text-sm transition-all ${
+                IsCoin ? "text-blue-500" : "text-gray-500"
+              }`}
+              onClick={() => ActiveCoin()}
+            >
+              <i className="fa-solid fa-globe ms-1"></i>
+              <p className="capitalize">{Coin || "USD"}</p>
+              <i
+                className={`fa-solid fa-angle-down transition-all duration-300 ${
+                  IsCoin ? "rotate-180" : "rotate-0"
+                }`}
+              ></i>
+
+              {IsCoin ? (
+                <div className="absolute mt-10 left-[-175px] w-[250px] p-3 bg-white shadow-md outline outline-1 outline-gray-100 rounded-xl">
+                  <ul className="flex flex-col text-lg text-end">
+                    {coins_type &&
+                      coins_type.map((e) => (
+                        <li
+                          onClick={() => setCoin(e)}
+                          className={`w-full py-3 transition-all ${
+                            Coin == e ? "text-blue-600" : "text-gray-500"
+                          } font-semibold hover:bg-blue-100 hover:text-blue-600 px-4 rounded-lg `}
+                        >
+                          {e}
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              ) : (
+                ""
+              )}
+            </button>
+          </div>
+          {/* Avatar */}
+          <button
+            onClick={(e) => ActiveAvatar()}
+            className="flex items-baseline flex-row-reverse gap-3 p-1 px-2 shadow-md outline outline-1 outline-gray-100 rounded-full text-[10px] sm:text-sm md:text-xl lg:text-2xl"
+          >
+            <div className="flex items-center justify-center h-4 sm:h-6 md:h-8 lg:h-10 w-4 sm:w-6 md:w-8 lg:w-10 bg-gray-300 rounded-full text-white">
+              <i class="fa-regular fa-user"></i>
+            </div>
+            <i class="fa-solid fa-ellipsis text-gray-600"></i>
+
+            {/* Popup */}
+            {IsAvatar ? (
+              <div className="absolute mt-14 left-[111px] w-[350px] p-3 bg-white shadow-md outline outline-1 outline-gray-100 rounded-xl text-gray-600">
+                <ul className="flex flex-col text-lg text-end">
+                  <li
+                    className={`w-full py-3 transition-all font-semibold hover:bg-blue-100 hover:text-blue-600 px-4 rounded-lg `}
+                  >
+                    Login
+                  </li>
+                  <hr className="my-1" />
+                  <li
+                    className={`w-full py-3 transition-all font-semibold hover:bg-blue-100 hover:text-blue-600 px-4 rounded-lg mt-2`}
+                  >
+                    Get The App
+                  </li>
+                  <li
+                    className={`w-full py-3 transition-all font-semibold hover:bg-blue-100 hover:text-blue-600 px-4 rounded-lg`}
+                  >
+                    Contact Support
+                  </li>
+                  <li
+                    className={`w-full py-3 transition-all font-semibold hover:bg-blue-100 hover:text-blue-600 px-4 rounded-lg mb-2`}
+                  >
+                    We Are Hiring
+                  </li>
+                  <hr />
+                  <li
+                    className={`w-full py-3 transition-all font-semibold hover:bg-blue-100 hover:text-blue-600 px-4 rounded-lg mt-3`}
+                  >
+                    Sell Your Event With Us
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              ""
+            )}
+          </button>
+        </div>
+      </header>
+      <hr />
+    </>
+  );
+}
